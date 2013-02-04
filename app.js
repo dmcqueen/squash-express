e5315545e926c174da75020fa978e5f018019b4f
Dave McQueen <david.mcqueen@neudesic.com>
2013-02-04 15:17:47 -0800

    Added nconf and cleaned up

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -11,14 +10,13 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
+/*
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -26,6 +24,11 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
+*/
+
+/*app.use(function(err, req, res, next){
+  console.log("in error handler" + err.stack);
+});*/
 
 // Routes
 
