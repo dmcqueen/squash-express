6b9ca746bd19fe2ad1fce97204a6d1ce380e8b98
Dave McQueen <david.mcqueen@neudesic.com>
2013-02-04 17:28:36 -0800

    moved squash report call to the global error handler

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 /**
  * Module dependencies.
  */
+var git = require('git-rev')
+var Squash = require('squash-node').Squash;
+var squash = new Squash(); 
+
+var nconf = require('nconf');
+var configPath = __dirname + "/./config.json";
+
+console.log('loading settings from: ' + configPath);
+nconf.file({
+    file: configPath
+});
+
+git.long(function (rev) {
+    squash.configure({ APIHost: nconf.get("squash:apihost"),
+           APIKey: nconf.get("squash:apikey"),
+           environment: nconf.get("squash:env"),
+           revision: rev
+       });
+       console.log('Squash Config');
+       console.log(squash.options);
+});
 
 var express = require('express')
   , routes = require('./routes');
@@ -14,6 +35,13 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+
+  app.use(function(err, req, res, next){
+    console.log("in global error handler");
+    console.log(err.stack);
+    squash.report(err);
+    next();
+  });
 });
 
 // Routes
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,3 @@
-var git = require('git-rev')
-var SquashError = require('squash-node').SquashError;
-var Squash = require('squash-node').Squash;
-var squash = new Squash(); 
-
-var nconf = require('nconf');
-var configPath = __dirname + "/../config.json";
-
-console.log('loading settings from: ' + configPath);
-nconf.file({
-    file: configPath
-});
-
-git.long(function (rev) {
-  	squash.configure({ APIHost: nconf.get("squash:apihost"),
-           APIKey: nconf.get("squash:apikey"),
-           environment: nconf.get("squash:env"),
-           revision: rev
-       });
-       console.log('config with revision:'+rev);
-});
-
-
-var writeErr = function (res, err) {
-    squash.report(err);
-
-    res.send(500, {
-        message: err.toString()
-    });
-};
-
 exports.index = function(req, res){
-  writeErr(res, new SquashError("web problem"));
+  console.log(t);
 };
